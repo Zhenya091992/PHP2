c6fe68cd070dc2e995adc45c7de5533080b34ac4
zhenya091992 <zhenya091992@gmail.com>
2021-11-15 17:59:46 +0300

    php2

---
+++ App/Db.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+class Db
+{
+    protected $dbh;
+
+    public function __construct()
+    {
+        $this->dbh = new \PDO('mysql:host=127.0.0.1;dbname=test', 'root', 'root');
+    }
+
+    public function execute($sql, array $data = null)
+    {
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute($data);
+        return $res;
+    }
+
+    public function query($sql, $class, array $data = null)
+    {
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute($data);
+        if (false !== $res) {
+            return $sth->fetchAll(\PDO::FETCH_CLASS, $class);
+        }
+        return [];
+    }
+}
